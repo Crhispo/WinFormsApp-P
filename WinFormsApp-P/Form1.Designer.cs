4ef37758703a19ff826f9430b7e680a52512487f
Crhispo <cristianandrade2001@gmail.com>
2022-04-05 13:48:56 -0500

    Agregar archivos de proyecto.

---
+++ WinFormsApp-P/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WinFormsApp_P
+{
+    partial class Form_curso
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.label_nombre = new System.Windows.Forms.Label();
+            this.textBox_nombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.Location = new System.Drawing.Point(115, 345);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(75, 23);
+            this.btn_aceptar.TabIndex = 0;
+            this.btn_aceptar.Text = "Aceptar";
+            this.btn_aceptar.UseVisualStyleBackColor = true;
+            this.btn_aceptar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(595, 345);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 1;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.btn_cancelar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label_nombre
+            // 
+            this.label_nombre.AutoSize = true;
+            this.label_nombre.Location = new System.Drawing.Point(115, 64);
+            this.label_nombre.Name = "label_nombre";
+            this.label_nombre.Size = new System.Drawing.Size(54, 15);
+            this.label_nombre.TabIndex = 2;
+            this.label_nombre.Text = "Nombre:";
+            this.label_nombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_nombre
+            // 
+            this.textBox_nombre.Location = new System.Drawing.Point(175, 61);
+            this.textBox_nombre.Multiline = true;
+            this.textBox_nombre.Name = "textBox_nombre";
+            this.textBox_nombre.Size = new System.Drawing.Size(105, 23);
+            this.textBox_nombre.TabIndex = 3;
+            // 
+            // Form_curso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_nombre);
+            this.Controls.Add(this.label_nombre);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_aceptar);
+            this.Name = "Form_curso";
+            this.Text = "Formulario de curso C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_aceptar;
+        private Button btn_cancelar;
+        private Label label_nombre;
+        private TextBox textBox_nombre;
+    }
+}
---
+++ WinFormsApp-P/Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp_P
+{
+    public partial class Form_curso : Form
+    {
+        public Form_curso()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
